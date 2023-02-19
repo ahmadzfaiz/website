f28649e45951cf9945ff0db14ce5c67808834c9c
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2023-02-19 12:34:32 +0700

    Update signals.py

--- home/signals.py
+++ home/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 from .models import ActivityLog, WebEntry
 import requests
 import json
+from project.settings import DEBUG
 
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
@@ -98,128 +99,130 @@ def log_user_login_failed(sender, credentials, request, **kwargs):
 
 @receiver(user_logged_out)
 def log_user_logout(sender, request, user, **kwargs):
-  # FETCH DATA GEOIP
-  ip = request.META.get('REMOTE_ADDR')
-  res = requests.get(f'http://ip-api.com/json/{ip}').text
-  geoip = json.loads(res)
-  
-  # LOGIKA UNTUK JENIS ELEKTRONIK
-  if request.user_agent.is_mobile:
-    electronic = 'Smartphone'
-  elif request.user_agent.is_tablet:
-    electronic = 'Tablet'
-  elif request.user_agent.is_pc:
-    electronic = 'PC/Laptop'
-  else:
-    electronic = None
+  if not DEBUG:
+    # FETCH DATA GEOIP
+    ip = request.META.get('REMOTE_ADDR')
+    res = requests.get(f'http://ip-api.com/json/{ip}').text
+    geoip = json.loads(res)
 
-  # LOGIKA UNTUK TIPE DEVICE
-  if request.user_agent.device.family == None:
-    device_type = 'None'
-  else:
-    device_type=request.user_agent.device.family
+    # LOGIKA UNTUK JENIS ELEKTRONIK
+    if request.user_agent.is_mobile:
+      electronic = 'Smartphone'
+    elif request.user_agent.is_tablet:
+      electronic = 'Tablet'
+    elif request.user_agent.is_pc:
+      electronic = 'PC/Laptop'
+    else:
+      electronic = None
 
-  # LOGIKA UNTUK BRAND DEVICE 
-  if request.user_agent.device.brand == None:
-    device_brand = 'None'
-  else:
-    device_brand=request.user_agent.device.brand
+    # LOGIKA UNTUK TIPE DEVICE
+    if request.user_agent.device.family == None:
+      device_type = 'None'
+    else:
+      device_type=request.user_agent.device.family
 
-  # LOGIKA UNITUK MODEL DEVICE
-  if request.user_agent.device.model == None:
-    device_model = 'None'
-  else:
-    device_model=request.user_agent.device.model
+    # LOGIKA UNTUK BRAND DEVICE 
+    if request.user_agent.device.brand == None:
+      device_brand = 'None'
+    else:
+      device_brand=request.user_agent.device.brand
 
-  WebEntry.objects.create(
-    action='User Logout', 
-    ip=request.META.get('REMOTE_ADDR'),
-    electronic=electronic,
-    is_touchscreen=request.user_agent.is_touch_capable,
-    is_bot=request.user_agent.is_bot,
-    os_type=request.user_agent.os.family,
-    os_version=request.user_agent.os.version_string,
-    browser_type=request.user_agent.browser.family,
-    browser_version=request.user_agent.browser.version_string,
-    device_type=device_type,
-    device_brand=device_brand,
-    device_model=device_model,
-    username=user.username,
-    country_code=geoip['countryCode'],
-    country=geoip['country'],
-    region_code=geoip['region'],
-    region=geoip['regionName'],
-    city=geoip['city'],
-    lat=geoip['lat'],
-    lon=geoip['lon'],
-    timezone=geoip['timezone'],
-    isp=geoip['isp'],
-    isp_detail=geoip['as'])
+    # LOGIKA UNITUK MODEL DEVICE
+    if request.user_agent.device.model == None:
+      device_model = 'None'
+    else:
+      device_model=request.user_agent.device.model
+
+    WebEntry.objects.create(
+      action='User Logout', 
+      ip=request.META.get('REMOTE_ADDR'),
+      electronic=electronic,
+      is_touchscreen=request.user_agent.is_touch_capable,
+      is_bot=request.user_agent.is_bot,
+      os_type=request.user_agent.os.family,
+      os_version=request.user_agent.os.version_string,
+      browser_type=request.user_agent.browser.family,
+      browser_version=request.user_agent.browser.version_string,
+      device_type=device_type,
+      device_brand=device_brand,
+      device_model=device_model,
+      username=user.username,
+      country_code=geoip['countryCode'],
+      country=geoip['country'],
+      region_code=geoip['region'],
+      region=geoip['regionName'],
+      city=geoip['city'],
+      lat=geoip['lat'],
+      lon=geoip['lon'],
+      timezone=geoip['timezone'],
+      isp=geoip['isp'],
+      isp_detail=geoip['as'])
 
 # LOGGING AKTIVITAS
 def log_activity(request):
-  # FETCH DATA GEOIP
-  ip = request.META.get('REMOTE_ADDR')
-  res = requests.get(f'http://ip-api.com/json/{ip}').text
-  geoip = json.loads(res)
+  if not DEBUG:
+    # FETCH DATA GEOIP
+    ip = request.META.get('REMOTE_ADDR')
+    res = requests.get(f'http://ip-api.com/json/{ip}').text
+    geoip = json.loads(res)
 
-  # LOGIKA UNTUK JENIS ELEKTRONIK
-  if request.user_agent.is_mobile:
-    electronic = 'Smartphone'
-  elif request.user_agent.is_tablet:
-    electronic = 'Tablet'
-  elif request.user_agent.is_pc:
-    electronic = 'PC/Laptop'
-  else:
-    electronic = None
+    # LOGIKA UNTUK JENIS ELEKTRONIK
+    if request.user_agent.is_mobile:
+      electronic = 'Smartphone'
+    elif request.user_agent.is_tablet:
+      electronic = 'Tablet'
+    elif request.user_agent.is_pc:
+      electronic = 'PC/Laptop'
+    else:
+      electronic = None
 
-  # LOGIKA UNTUK TIPE DEVICE
-  if request.user_agent.device.family == None:
-    device_type = 'None'
-  else:
-    device_type=request.user_agent.device.family
+    # LOGIKA UNTUK TIPE DEVICE
+    if request.user_agent.device.family == None:
+      device_type = 'None'
+    else:
+      device_type=request.user_agent.device.family
 
-  # LOGIKA UNTUK BRAND DEVICE 
-  if request.user_agent.device.brand == None:
-    device_brand = 'None'
-  else:
-    device_brand=request.user_agent.device.brand
+    # LOGIKA UNTUK BRAND DEVICE 
+    if request.user_agent.device.brand == None:
+      device_brand = 'None'
+    else:
+      device_brand=request.user_agent.device.brand
 
-  # LOGIKA UNITUK MODEL DEVICE
-  if request.user_agent.device.model == None:
-    device_model = 'None'
-  else:
-    device_model=request.user_agent.device.model
+    # LOGIKA UNITUK MODEL DEVICE
+    if request.user_agent.device.model == None:
+      device_model = 'None'
+    else:
+      device_model=request.user_agent.device.model
 
-  # LOGIKA UNTUK USERNAME
-  if request.user.is_authenticated:
-    username = request.user.username
-  else:
-    username = 'User not login'
+    # LOGIKA UNTUK USERNAME
+    if request.user.is_authenticated:
+      username = request.user.username
+    else:
+      username = 'User not login'
 
-  activity = ActivityLog.objects.create(
-    url_access=request.build_absolute_uri(), 
-    ip=ip,
-    electronic=electronic,
-    is_touchscreen=request.user_agent.is_touch_capable,
-    is_bot=request.user_agent.is_bot,
-    os_type=request.user_agent.os.family,
-    os_version=request.user_agent.os.version_string,
-    browser_type=request.user_agent.browser.family,
-    browser_version=request.user_agent.browser.version_string,
-    device_type=device_type,
-    device_brand=device_brand,
-    device_model=device_model,
-    username=username,
-    country_code=geoip['countryCode'],
-    country=geoip['country'],
-    region_code=geoip['region'],
-    region=geoip['regionName'],
-    city=geoip['city'],
-    lat=geoip['lat'],
-    lon=geoip['lon'],
-    timezone=geoip['timezone'],
-    isp=geoip['isp'],
-    isp_detail=geoip['as'])
+    activity = ActivityLog.objects.create(
+      url_access=request.build_absolute_uri(), 
+      ip=ip,
+      electronic=electronic,
+      is_touchscreen=request.user_agent.is_touch_capable,
+      is_bot=request.user_agent.is_bot,
+      os_type=request.user_agent.os.family,
+      os_version=request.user_agent.os.version_string,
+      browser_type=request.user_agent.browser.family,
+      browser_version=request.user_agent.browser.version_string,
+      device_type=device_type,
+      device_brand=device_brand,
+      device_model=device_model,
+      username=username,
+      country_code=geoip['countryCode'],
+      country=geoip['country'],
+      region_code=geoip['region'],
+      region=geoip['regionName'],
+      city=geoip['city'],
+      lat=geoip['lat'],
+      lon=geoip['lon'],
+      timezone=geoip['timezone'],
+      isp=geoip['isp'],
+      isp_detail=geoip['as'])
 
-  return activity
+    return activity

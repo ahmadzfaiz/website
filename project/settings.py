790e3356e5928be122f3f85c79bc2c5022295fc7
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2023-03-24 00:47:27 +0700

    Update Staticfiles

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     # IMPORTED APPS
     'admin_interface',
     'colorfield',
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -136,8 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 MEDIA_URL = '/media/'

7c1e816fe3280bf119cadcd98dd6bc3475224b5f
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2023-03-23 17:27:44 +0700

    Update views.py

--- home/views.py
+++ home/views.py
@@ -22,17 +22,17 @@ def home(request):
     if contact.is_valid():
       item = contact.save(commit= False)
       item.user = request.user
-      item.save()
+      # item.save()
 
-      html = render_to_string('home/email.html', {
-        'name': item.name,
-        'email': item.email,
-        'phone': item.phone_number,
-        'message': item.message
-      }) 
+      # html = render_to_string('home/email.html', {
+      #   'name': item.name,
+      #   'email': item.email,
+      #   'phone': item.phone_number,
+      #   'message': item.message
+      # }) 
 
-      sent_to = [item.email, 'ahmadzfaiz.gcp@gmail.com']
-      send_mail(item.name, item.message, 'ahmadzfaiz.gcp@gmail.com', sent_to, html_message= html, fail_silently=False)
+      # sent_to = [item.email, 'ahmadzfaiz.gcp@gmail.com']
+      # send_mail(item.name, item.message, 'ahmadzfaiz.gcp@gmail.com', sent_to, html_message= html, fail_silently=False)
       return redirect('home')
   
   else:
